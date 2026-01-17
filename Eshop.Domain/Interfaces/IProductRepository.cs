62b93af0f1debb05f0f6a56ccc25314a29a76347
rehousekl <rehousek@seznam.cz>
2026-01-17 20:09:34 +0100

    Domain - Product

---
+++ Eshop.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Eshop.Domain.Entities;
+
+namespace Eshop.Domain.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllAsync();
+
+    Task<Product?> GetByIdAsync(Guid id);
+
+    Task AddAsync(Product product);
+
+    Task UpdateAsync(Product product);
+
+    Task SaveChangesAsync();
+}
