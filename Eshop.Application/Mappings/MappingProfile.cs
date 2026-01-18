bf6959669fe9379e0c8d713ffb584bf4eb4dae43
rehousekl <rehousek@seznam.cz>
2026-01-18 16:11:16 +0100

    Application - Mapping

---
+++ Eshop.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Eshop.Application.DTO.Response;
+using Eshop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Application.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Product, ProductResponseDto>();
+        // Pokud by se názvy vlastností lišily, použijte .ForMember(...)
+    }
+}
