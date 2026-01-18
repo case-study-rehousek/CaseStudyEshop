69a51d03614f150a3cd6167c3268344b8bac02a7
rehousekl <rehousek@seznam.cz>
2026-01-18 11:33:35 +0100

    Product service - implementation

--- Eshop.Application/Services/ProductService.cs
+++ Eshop.Application/Services/ProductService.cs
@@ -1,18 +1,65 @@
-﻿using Eshop.Application.DTO.Request;
+﻿using AutoMapper;
+using Eshop.Application.DTO.Request;
 using Eshop.Application.DTO.Response;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Eshop.Application.Interfaces;
+using Eshop.Domain.Entities;
+using Eshop.Domain.Interfaces;
+
 
 namespace Eshop.Application.Services;
 
-public interface IProductService
+public class ProductService : IProductService
 {
-    Task<IEnumerable<ProductResponseDto>> GetAllAsync();
-    Task<ProductResponseDto?> GetByIdAsync(Guid id);
-    Task<Guid> CreateAsync(CreateProductRequestDto dto);
-    Task<bool> UpdateStockAsync(Guid id, UpdateStockRequestDto dto);
+    private readonly IProductRepository _productRepository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IProductRepository productRepository, IMapper mapper)
+    {
+        _productRepository = productRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<ProductResponseDto?> GetByIdAsync(Guid id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+        return product == null ? null : _mapper.Map<ProductResponseDto>(product);
+    }
+
+    public async Task<IEnumerable<ProductResponseDto>> GetAllAsync()
+    {
+        var products = await _productRepository.GetAllAsync();
+        return _mapper.Map<IEnumerable<ProductResponseDto>>(products);
+    }
+
+    public async Task<Guid> CreateAsync(CreateProductRequestDto request)
+    {
+        var product = new Product
+        {
+            Id = Guid.NewGuid(),
+            Name = request.Name,
+            Price = request.Price,
+            StockQuantity = request.StockQuantity,
+            ImageUrl = request.ImageUrl,
+            Description = request.Description
+        };
+
+        await _productRepository.AddAsync(product);
+        await _productRepository.SaveChangesAsync();
+
+        return product.Id;
+    }
+
+    public async Task<bool> UpdateStockAsync(Guid productId, UpdateStockRequestDto request)
+    {
+        var product = await _productRepository.GetByIdAsync(productId);
+        if (product == null) return false;
+
+        product.StockQuantity = request.NewQuantity;
+
+        await _productRepository.UpdateAsync(product);
+        await _productRepository.SaveChangesAsync();
+
+        return true;
+    }
 }
 
